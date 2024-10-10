ad272773260644d37ccf374ab4dd99267175b299
Om-83525 <omdhavale2001@gmail.com>
2024-10-10 12:14:30 +0530

    validations are added on the Data transfer Object

--- spring_boot_backend_template/src/main/java/com/nimap/dto/ProductRequestDTO.java
+++ spring_boot_backend_template/src/main/java/com/nimap/dto/ProductRequestDTO.java
@@ -1,5 +1,10 @@
 package com.nimap.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +12,11 @@ import lombok.Setter;
 @Setter
 public class ProductRequestDTO {
 	private Long categoryId;
+	@NotBlank(message = "Product name can't be blank")
+	@Size(min = 3, max = 100, message = "Name must be between 3 to 100 characters")
 	private String name;
+	@NotNull(message = "Price can't be empty")
+	@Min(value = 1)
 	private double price;
 
 }
